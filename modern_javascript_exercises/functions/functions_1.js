e957b55c85f7d049d16b8e5168ad1e0dd5aa4955
DanielMurphy87 <84238673+DanielMurphy87@users.noreply.github.com>
2023-01-30 13:39:32 +0200

    Update functions_1.js

--- modern_javascript_exercises/functions/functions_1.js
+++ modern_javascript_exercises/functions/functions_1.js
@@ -16,14 +16,14 @@ people.forEach(logPerson);
 // Function in a Variable
 // Store the logPerson() function in a variable called performLogPerson
 
-const logPerson = function(person) {
+const performLogPerson = function(person) {
     console.log("The person is " + person);
 };
 
 // Arrow Functions in a Variable
 // Write a version of logPerson() as an Arrow Function and store it in a variable called arrowVersionOfLogPerson
 
-const logPerson = (person) => console.log(`The person is ${person}`);
+const arrowVersionOfLogPerson = (person) => console.log(`The person is ${person}`);
 
 // Anonymous Arrow Functions
 // Replace logPerson below with an Anonymous Arrow Function that does the same thing!
